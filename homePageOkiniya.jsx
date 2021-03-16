5b4cbdec7b63aee0758e1f31837aab24a978080f
Will-CG <80711569+Will-CG@users.noreply.github.com>
2021-03-16 16:56:33 +0000

    Add files via upload

---
+++ homePageOkiniya.jsx
@@ -0,0 +1,58 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./homePageOkiniya.css";
+
+class HomePageOkiniya extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="b16f774b-8421-4a1e-874d-a5860baa196a" className="homePageOkiniya">        <div data-layer="da013642-ec81-4f76-bfeb-4c03f3525c7e" className="untitled1"></div>
+        <div data-layer="3c24bad8-6c31-4f31-95c5-850f3f03f9b5" className="rectangle5"></div>
+        <div data-layer="ed936b0d-1faa-4f2d-800a-dd900f93d2ce" className="restaurants">Restaurants </div>
+        <svg data-layer="24f0ca89-fbe5-4f44-bba2-0c941f3b8cdb" preserveAspectRatio="none" viewBox="0 -0.5 162 1" className="line2"><path d="M 0 0 L 162 0"  /></svg>
+        <svg data-layer="aa137e8b-885d-4678-81ed-9db16cd8a7a5" preserveAspectRatio="none" viewBox="-0.75 -0.75 235.5 229.5" className="xf3f4ddf0"><defs><pattern id="img-xf3f4ddf0" patternContentUnits="userSpaceOnUse" width="100%" height="100%"><image xlinkHref={require('./assets/xf3f4ddf0.png')} x="0" y="0" width="234.00px" height="228.00px" /></pattern></defs><path d="M 117 0 C 181.6173095703125 0 234 51.0395393371582 234 114 C 234 176.9604644775391 181.6173095703125 228 117 228 C 52.3826904296875 228 0 176.9604644775391 0 114 C 0 51.0395393371582 52.3826904296875 0 117 0 Z" fill="url(#img-xf3f4ddf0)" /></svg>
+        <svg data-layer="e61519a8-d7df-4020-8563-c9d6c3f5d2ac" preserveAspectRatio="none" viewBox="-0.75 -0.75 236.5 230.5" className="logo3"><defs><pattern id="img-logo3" patternContentUnits="userSpaceOnUse" width="100%" height="100%"><image xlinkHref={require('./assets/logo3.png')} x="0" y="0" width="235.00px" height="229.00px" /></pattern></defs><path d="M 117.5 0 C 182.3934631347656 0 235 51.26340103149414 235 114.5 C 235 177.7366027832031 182.3934631347656 229 117.5 229 C 52.60654449462891 229 0 177.7366027832031 0 114.5 C 0 51.26340103149414 52.60654449462891 0 117.5 0 Z" fill="url(#img-logo3)" /></svg>
+        <div data-layer="dbfc440b-73f4-4add-83dc-a4439a4edcb3" className="hongdaePocha">Hong’Dae Pocha</div>
+        <div data-layer="5c51c17e-c59a-4414-a6c7-362e620c393b" className="shiBuYa">Shi Bu Ya</div>
+        <div data-layer="4267dbc8-e30e-44c1-a7a2-fe02dec78bef" className="okiniyaLtd2021sovereignHouseGrahamRoadLondonHa35rfGlDesigns">Okiniya LTD 2021<br />Sovereign House, Graham Road, London, HA3 5RF<br /><br />· GL Designs ·</div>
+        <div data-layer="f6f118a9-0fc7-48ef-95c3-06e3f4be093f" className="hongdaeChicken">Hong’Dae Chicken</div>
+        <div data-layer="1399bf13-afcf-4250-b549-f9f3dd36f4f4" className="hanwooSikDang">Hanwoo Sik Dang</div>
+        <svg data-layer="8aef2725-7f62-49c4-9c34-745ef45aad0a" preserveAspectRatio="none" viewBox="-0.75 -0.75 236.5 230.5" className="hongdaePochaLogo"><defs><pattern id="img-hongdaePochaLogo" patternContentUnits="userSpaceOnUse" width="100%" height="100%"><image xlinkHref={require('./assets/hongdaePochaLogo.png')} x="0" y="0" width="235.00px" height="229.00px" /></pattern></defs><path d="M 117.5 0 C 182.3934631347656 0 235 51.26340103149414 235 114.5 C 235 177.7366027832031 182.3934631347656 229 117.5 229 C 52.60654449462891 229 0 177.7366027832031 0 114.5 C 0 51.26340103149414 52.60654449462891 0 117.5 0 Z" fill="url(#img-hongdaePochaLogo)" /></svg>
+        <svg data-layer="e4ce4ba3-afaa-4d27-bd16-ea2d745f67f7" preserveAspectRatio="none" viewBox="-0.75 -0.75 236.5 230.5" className="hongdaelogo03"><defs><pattern id="img-hongdaelogo03" patternContentUnits="userSpaceOnUse" width="100%" height="100%"><image xlinkHref={require('./assets/hongdaelogo03.png')} x="0" y="0" width="235.00px" height="229.00px" /></pattern></defs><path d="M 117.5 0 C 182.3934631347656 0 235 51.26340103149414 235 114.5 C 235 177.7366027832031 182.3934631347656 229 117.5 229 C 52.60654449462891 229 0 177.7366027832031 0 114.5 C 0 51.26340103149414 52.60654449462891 0 117.5 0 Z" fill="url(#img-hongdaelogo03)" /></svg>
+        <div data-layer="a61842d9-953e-4bef-a190-1995fdc1d5b6" className="rectangle3"></div>
+        <div data-layer="af41e360-2085-474b-a578-a2b7b4125dfa" className="group5">            <svg data-layer="972f4ab3-d0d3-4df2-9267-d9a7ff5eab9a" preserveAspectRatio="none" viewBox="0 -0.5 134 1" className="line3"><path d="M 0 0 L 134 0"  /></svg>
+            <div data-layer="24677eb0-1495-4ace-aeb5-0d97982c24dd" className="atOkiniyaLtdWeEnterpriseInJapaneseAndKoreanBasedRestaurantsAcrossCentralLondonsinceOpeningIn2015OurGroupHasProgressedRapidlyThroughoutTheYearsSomethingSomethingSomethingAboutNewStoresBlahBlahBlahourAimIsToExpa">At Okiniya LTD, we enterprise in Japanese and Korean-based restaurants across central <br />London.<br /><br />Since opening in 2015, our group has progressed rapidly throughout the years; <br />Something something something about new stores blah blah blah<br /><br />Our aim is to expand out across the UK and continue to provide the best quality foods <br />and services for our customers, consistently. <br /><br />More and More and More stuffs .<br /><br />Yeah yeah ?</div>
+            <div data-layer="f753ed54-a588-41ef-bd71-9a8b4025bf03" className="ourTeam">Our Team</div>
+</div>
+        <div data-layer="5d1689b2-5f6f-4faf-8203-325573e0798c" className="group3">            <div data-layer="9f438035-25c9-4566-99c5-6d41d6b1c4c2" className="topBanner">                <svg data-layer="c1adff9d-520e-4e6f-8107-eea3b543d1dc" preserveAspectRatio="none" viewBox="0 0 1920 202" className="path1"><path d="M 0 0 L 1920 0 L 1920 202 L 0 202 L 0 0 Z"  /></svg>
+                <svg data-layer="411bb2f7-a854-4396-b0d0-b229c7ff26d9" preserveAspectRatio="none" viewBox="0 -0.5 289 1" className="line1"><path d="M 0 0 L 289 0"  /></svg>
+                <div data-layer="70897f67-8331-4e59-8998-617f48ee3fb7" className="oKINIYALTD"><span className="oKINIYALTD-0">O K I N I Y A</span><span className="oKINIYALTD-13">  </span><span className="oKINIYALTD-15">L T D</span></div>
+</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+HomePageOkiniya.propTypes = {
+
+}
+
+HomePageOkiniya.defaultProps = {
+
+}
+
+
+export default HomePageOkiniya;
+          
